c47aa8424e847915c232482424ed51a8c7eef90e
張芷銓 <ccchang@iii.org.tw>
2023-09-06 10:49:43 +0800

    add streamlit ui

---
+++ interface/res_page.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import akasha
+import sys
+import os
+sys.path.append('../')
+
+
+
+def response_page():
+    st.title("Get Response")
+    st.markdown('##')
+    st.markdown('##')
+    
+    response_board, para_set = st.columns([2,1])
+    
+    
+            
+    with para_set:
+        doc_path = st.selectbox("Document Path", st.session_state.docs_list, index=0, help="The path of the document folder.")
+        final_doc_path = st.session_state.docs_path + '/' + doc_path
+        embed = st.selectbox("Embedding Model", st.session_state.embed_list, index=0, help="The embedding model used to embed documents.")
+        model = st.selectbox("Model", st.session_state.model_list, index=0, help="The model used to generate response.")
+        cks, tpk = st.columns([1,1])
+        with cks:
+            chunksize = st.number_input("Chunk Size", value=500, min_value = 100, max_value = 2000, step = 100, help="The size of each chunk of the document.")
+        with tpk:
+            topK = st.number_input("Top K", value=2, min_value = 1, max_value = 10, step = 1, help="The number of top relevant chunks to be selected from documents.")
+        
+            
+        seat, thre = st.columns([1,1])
+        with seat:
+            search_type = st.selectbox("Search Type", st.session_state.search_list, index=0, help="The search method used to select top relevant chunks.")
+        with thre:
+            threshold = st.number_input("Threshold",value= 0.2, min_value=0.1, max_value=0.9, step=0.05, help="The threshold used to select top relevant chunks.")
+        
+        prompt = st.text_area("Prompt","" , help="The prompt you want to ask the model.")
+        sb1, sb2 = st.columns([1, 1])
+        with sb1:
+            if st.button("Clear", type="primary", use_container_width=True, help="Clear the prompt and response."):
+                st.session_state.prompt_list = []
+                st.session_state.response_list = []
+                st.experimental_rerun()
+        with sb2:
+            if st.button("Submit", type="primary", use_container_width=True):
+                if st.session_state.openai_key != "": 
+                    os.environ["OPENAI_API_KEY"] = st.session_state.openai_key
+                ans = akasha.get_response(final_doc_path,prompt, embed, chunksize, model, False, topK, threshold, 'ch', search_type)
+                st.session_state.prompt_list.append(prompt)
+                st.session_state.response_list.append(ans)
+    
+    
+    with response_board:
+        
+        for i in range(len(st.session_state.response_list)):
+            with st.chat_message("user"):
+                st.markdown(st.session_state.prompt_list[i])
+            with st.chat_message("assistant"):
+                st.markdown(st.session_state.response_list[i])
+    
---
+++ interface/upload_file.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from pathlib import Path
+
+def up_load():
+    
+    st.session_state.openai_key = st.text_input("Openai Key", type="password")
+    
+    
+    
+    uploaded_files = st.file_uploader("Upload document files", accept_multiple_files=True,type=['txt', 'pdf', 'docx'])
+    
+    path_name = st.text_input("path name")
+    sb1,sb2,sb3 = st.columns([1,1,1])
+    with sb2:
+        submit_but = st.button("Submit", type="primary",use_container_width=True)
+    if submit_but:
+        if path_name in st.session_state.docs_list:
+            st.error("Path name already exist, please use another path name.")
+        
+        else:
+            # create path
+            if path_name[-1]!='/':
+                path_name = path_name + '/'
+            save_path = Path(st.session_state.docs_path, path_name)
+            # check if path exist
+            if not save_path.exists():
+                save_path.mkdir(parents=True)
+                st.session_state.docs_list.append(path_name[:-1])    
+            ### write file to doc folder ###
+            for uploaded_file in uploaded_files:
+                bytes_data = uploaded_file.read()
+                
+                with open(save_path.joinpath(uploaded_file.name), "wb") as f:
+                    f.write(bytes_data)
+
+                st.write("uploaded file:", uploaded_file.name)
+        
+        print(st.session_state.docs_list)
