4c68b34ce3359a0b8302e197065c5dd92435059b
張芷銓 <ccchang@iii.org.tw>
2023-10-26 15:07:53 +0800

    fix opencc version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='akasha',
                    'tiktoken==0.4.0','lark==1.1.7','scikit-learn<1.3.0','jieba==0.42.1',\
                    'sentence-transformers==2.2.2', 'torch==2.0.1', 'transformers==4.31.0',\
                         'llama-cpp-python==0.2.6','auto-gptq==0.3.1','tqdm==4.65.0', "docx2txt==0.8", "rouge==1.0.1",\
-                              "rouge-chinese==1.0.3","bert-score==0.3.13", "opencc","click","tokenizers==0.13.3"],
+                              "rouge-chinese==1.0.3","bert-score==0.3.13", "opencc==1.1.6","click","tokenizers==0.13.3"],
       packages=['akasha', 'akasha.models','cli','akasha.eval','akasha.summary'],
       entry_points={
             'console_scripts': [
