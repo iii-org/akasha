099a7b467cde026a26fa49409a6a975cc63adca7
張芷銓 <ccchang@iii.org.tw>
2023-12-08 14:55:01 +0800

    add dsrpt and change openai version in setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 import platform
 
 install_requires = [
-    "pypdf==3.12.2",
+    "pypdf",
     "langchain==0.0.345",
     "chromadb==0.4.14",
-    "openai==0.27.8",
-    "tiktoken==0.4.0",
+    "openai==0.27",
+    "tiktoken",
     "lark==1.1.7",
     "scikit-learn<1.3.0",
     "jieba==0.42.1",
@@ -34,9 +34,13 @@ setup(
     name="akasha",
     version="0.8",
     description="document QA package using langchain and chromadb",
+    long_description="""Akasha simplifies document-based Question Answering (QA) by harnessing the power of Large Language Models to accurately answer your queries while searching through your provided documents.
+With Akasha, you have the flexibility to choose from a variety of language models, embedding models, and search types. Adjusting these parameters is straightforward, allowing you to optimize your approach and discover the most effective methods for obtaining accurate answers from Large Language Models.""",
     author="chih chuan chang",
+    url="https://gitlab-devops.iii.org.tw/root/qaiii-1",
     author_email="ccchang@iii.org.tw",
     install_requires=install_requires,
     packages=["akasha", "akasha.models", "cli", "akasha.eval", "akasha.summary", "akasha.interface"],
     entry_points={"console_scripts": ["akasha = cli.glue:akasha"]},
+    python_requires=">=3.8"
 )
