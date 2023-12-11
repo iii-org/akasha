d123c2fc4ab19bb3e14b55e11aaaf22f1b6610be
張芷銓 <ccchang@iii.org.tw>
2023-12-11 16:35:08 +0800

    set up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import platform
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 
 install_requires = [
