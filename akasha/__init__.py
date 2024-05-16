e8976c0099e7eb8aa85194ff5ae0484b399ba191
張芷銓 <ccchang@iii.org.tw>
2024-05-16 16:18:42 +0800

    change module name

--- akasha/__init__.py
+++ akasha/__init__.py
@@ -1,4 +1,4 @@
-from .akasha import *
+from .akashas import *
 from .agents import *
 from .helper import *
 from .summary import *
--- akasha/prompts.py
+++ akasha/prompts.py
@@ -1,5 +1,5 @@
 from typing import List, Union, Tuple
-import format as afr
+import akasha.format as afr
 
 sys_s = "[INST] <<SYS>> "
 sys_e = " <<SYS>> [/INST]\n\n"
