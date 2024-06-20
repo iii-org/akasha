4f4e04ce9ea9adf2fc475ec9d2badbdd77dc11fb
張芷銓 <ccchang@iii.org.tw>
2024-06-20 16:04:47 +0800

    update version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name="akasha-terminal",
-    version="0.8.36",
+    version="0.8.37",
     description="document QA package using langchain and chromadb",
     long_description=long_description,
     long_description_content_type="text/markdown",
