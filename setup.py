5d6ca66fce68a3f38e724f5d698ee7c7d7007b15
張芷銓 <ccchang@iii.org.tw>
2025-01-09 12:03:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ install_requires.append("torchvision==0.15.2; platform_system=='Darwin'")
 install_requires.append("torchvision==0.17.0; platform_system=='Linux'")
 setup(
     name="akasha_terminal",
-    version="0.8.85",
+    version="0.8.86",
     description="document QA(RAG) package using langchain and chromadb",
     long_description=long_description,
     long_description_content_type="text/markdown",
