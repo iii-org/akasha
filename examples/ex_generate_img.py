d8cb288e3b40ef545357043de0ce8d67cada8633
張芷銓 <ccchang@iii.org.tw>
2025-05-14 18:17:01 +0800

    update example explain

--- examples/ex_generate_img.py
+++ examples/ex_generate_img.py
@@ -2,7 +2,7 @@ import akasha
 
 ### currently only support openai/azure openai  and gemini (model="gemini:gemini-2.0-flash-preview-image-generation") ###
 # please noted that your openai account may need verification to use protected model like "gpt-image-1"
-
+# also, if you are using azure openai, please make sure your AZURE_API_VERSION is set to "2025-04-01-preview" in your .env file
 ###  generate image with prompt, you can select "high", "medium", "low" quality or size like "256x256", "512x512", "1024x1024"
 save_path = akasha.gen_image(
     prompt="一隻可愛的絨毛娃娃，是北海道的長尾山雀，坐在白雪的樹枝上唱歌",
